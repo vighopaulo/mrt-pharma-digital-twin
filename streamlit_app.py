c51cb25c37dc112335d8eff103167dc20c047f2a
vighopaulo <vighopaulo@gmail.com>
2026-07-19 15:33:58 -0400

    Add Streamlit smoke test

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="MRT Pharma V6", layout="wide")
+
+st.title("🚀 MRT Pharma Digital Twin")
+
+st.success("Streamlit deployment is working.")
+
+st.write("If you can see this page, the deployment is correctly configured.")
+
+st.info("Next step: connect the Digital Twin simulation engine.")
